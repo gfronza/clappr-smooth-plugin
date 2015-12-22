aec11d2a73ff7212a126ff8c36fa793527d28ab8
Germano Fronza <germano@eventials.com>
2015-12-22 17:11:32 -0200

    Webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,10 @@
 var path = require('path'),
     webpack = require('webpack'),
     TransferWebpackPlugin = require('transfer-webpack-plugin'),
-    minimize = process.argv.indexOf('--no-minimize') === -1 ? true : false,
-    plugins = [],
     filename = 'smooth.js';
-
-if (minimize) {
-    plugins.push(new webpack.optimize.UglifyJsPlugin({minimize: true}));
-    filename = 'smooth.min.js';
-}
 
 module.exports = {
     entry: path.resolve(__dirname, 'index.js'),
-    devtool: minimize ? "source-map" : "",
     externals: {
         clappr: 'Clappr',
     },
@@ -31,7 +23,6 @@ module.exports = {
     resolve: {
         extensions: ['', '.js'],
     },
-    plugins: plugins,
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: filename,
